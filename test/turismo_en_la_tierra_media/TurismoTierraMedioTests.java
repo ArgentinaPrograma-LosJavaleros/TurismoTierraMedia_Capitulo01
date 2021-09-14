4c51f6caf2c8175670a1b1f38feec1f9374cf97d
lucas-aquino <luckasakino@protonmail.com>
2021-09-14 15:31:30 -0300

    Se Agrego un test

---
+++ test/turismo_en_la_tierra_media/TurismoTierraMedioTests.java
@@ -0,0 +1,24 @@
+package turismo_en_la_tierra_media;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TurismoTierraMedioTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
